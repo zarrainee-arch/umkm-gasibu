e9cc210dbd3bf38f4fdebbf6faa3f27ee14cd74d
Zahra Qurrota Aini <rvksmtv5zj@privaterelay.appleid.com>
2026-06-09 21:30:23 +0700

    Setup PHP project structure

---
+++ config/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "db_umkm_gasibu";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'config/koneksi.php';
+
+echo "<h1>UMKM Gasibu</h1>";
+
+if ($koneksi) {
+    echo "<p>Koneksi database berhasil!</p>";
+}
+
+?>
